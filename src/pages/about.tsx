ffb09fe32c1a1de910765d9e589e3fb56562c6e8
Amox DANSOU <amoxdansou63@gmail.com>
2024-03-05 08:13:07 +0100

    exo nextApp

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,9 +1,11 @@
 import Header from "./conpoment/header";
+import Exemple from "./conpoment/exemple";
 export default function About(){
     return(
         <div>
             <Header/>
             <h1>about</h1>
+            <Exemple/>
         </div>
     )
 } 
---
+++ src/pages/conpoment/exemple.tsx
@@ -0,0 +1,9 @@
+import style from "../../styles/exemple.module.css"
+export default function Exemple(){
+    return(
+        <div className={style.compo}>
+            <h1>Titre du compsant</h1>
+            <p>Comtenu du compsant</p>
+        </div>
+    )
+}
